0a80ab4477536be26d661fd0c318098ffcd8c44c
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-20 19:01:40 +0530

    Work to do

--- app/src/main/java/apps/everythingforward/com/wellnesstherapist/TherapistProfile.java
+++ app/src/main/java/apps/everythingforward/com/wellnesstherapist/TherapistProfile.java
@@ -1,12 +1,25 @@
 package apps.everythingforward.com.wellnesstherapist;
 
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -34,11 +47,16 @@ public class TherapistProfile extends AppCompatActivity {
     String imagePath;
     String profileNameStr;
     String profileDescriptionStr;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_therapist_profile);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
 
 
         profileName = (TextView)findViewById(R.id.therapistProfileName);
@@ -67,6 +85,8 @@ public class TherapistProfile extends AppCompatActivity {
         Picasso.with(getApplicationContext()).load(uri).fit().centerCrop().into(profileImage);
 
         getNameAndDesc();
+
+        setUpDrawer();
     }
 
     private void getNameAndDesc()
@@ -92,5 +112,44 @@ public class TherapistProfile extends AppCompatActivity {
 
 
 
+    }
+
+
+
+    private void setUpDrawer()
+    {
+
+
+
+
+
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("My Profile");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("My Patients");
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+
+                .withHeaderBackground(getResources().getDrawable(R.drawable.headerbg))
+                .addProfiles(
+                        new ProfileDrawerItem().withName(profileNameStr).withEmail(ParseUser.getCurrentUser().getEmail())
+                )
+                .build();
+
+
+//create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2
+
+                )
+
+                .build();
     }
 }
