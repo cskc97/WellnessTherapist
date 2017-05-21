ec6aca26ea93de9d0fa0a61fa6802c4486ca6cb1
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-21 20:21:50 +0530

    Almost done

--- app/src/main/java/apps/everythingforward/com/wellnesstherapist/PatientsActivity.java
+++ app/src/main/java/apps/everythingforward/com/wellnesstherapist/PatientsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -32,6 +33,9 @@ public class PatientsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patients);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Patients");
 
         passData = new ArrayList<ParseObject>();
         recyclerView = (RecyclerView)findViewById(R.id.patientsRV);
